cb3c1f25cba3d4a2ff0c3e5d7af3b588e9176d58
animesh <royanimesh2211@gmail.com>
2024-08-12 11:40:26 +0530

    Automated Unit and UI Test

---
+++ SampleApps/AutomatedTestTipTime.kt
@@ -0,0 +1,54 @@
+// Local Unit Test
+package com.appmakerszone.tiptime
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import java.text.NumberFormat
+
+class TipCalculatorTests {
+
+    @Test
+    fun calculateTip_20PercentNoRoundup() {
+        val amount = 10.00
+        val tipPercent = 20.00
+        val expectedTip = NumberFormat.getCurrencyInstance().format(2)
+        val actualTip = calculateTip(amount = amount, tipPercent = tipPercent, false)
+        assertEquals(expectedTip, actualTip)
+    }
+}
+
+// Instrumented UI Test
+package com.appmakerszone.tiptime
+
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performTextInput
+import com.appmakerszone.tiptime.ui.theme.TipTimeTheme
+import org.junit.Rule
+import org.junit.Test
+import java.text.NumberFormat
+
+class TipUITests {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun calculate_20_percent_tip() {
+
+        composeTestRule.setContent {
+            TipTimeTheme {
+                TipTimeLayout()
+            }
+        }
+        composeTestRule.onNodeWithText("Bill Amount")
+            .performTextInput("10")
+        composeTestRule.onNodeWithText("Tip Percentage")
+            .performTextInput("20")
+        val expectedTip = NumberFormat.getCurrencyInstance().format(2)
+        composeTestRule.onNodeWithText("Tip Amount: $expectedTip").assertExists(
+            "No node with this text was found."
+        )
+
+    }
+}
