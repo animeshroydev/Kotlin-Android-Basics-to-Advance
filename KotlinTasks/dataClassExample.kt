fc98434472ab9cd19e8af52d58049dbcc9c99f6c
animesh <royanimesh2211@gmail.com>
2024-08-18 00:57:06 +0530

    using data class example

---
+++ KotlinTasks/dataClassExample.kt
@@ -0,0 +1,23 @@
+enum class Difficulty {
+    EASY, MEDIUM, HARD
+}
+
+data class Question<T>(
+    val questionText: String,
+    val answer: T,
+    val difficulty: Difficulty
+)
+  
+    fun main() {
+    val question1 = Question<String>("Quoth the raven ___", "nevermore", Difficulty.MEDIUM)
+    val question2 = Question<Boolean>("The sky is green. True or false", false, Difficulty.EASY)
+    val question3 = Question<Int>("How many days are there between full moons?", 28, Difficulty.HARD)
+    
+       println(question1.answer)
+       println(question1.difficulty)
+       println(question2.answer)
+       println(question3.answer)
+       
+        println(question1.toString())
+    
+}
