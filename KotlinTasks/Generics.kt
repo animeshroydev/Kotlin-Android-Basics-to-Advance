e90dab2d3c3d6656067ec6024a6d4a0b1040951f
animesh <royanimesh2211@gmail.com>
2024-08-17 23:29:53 +0530

    generics example

--- KotlinTasks/Generics.kt
+++ KotlinTasks/Generics.kt
@@ -16,3 +16,23 @@ class NumericQuestion(
     val answer: Int,
     val difficulty: String
 )
+
+// using generics
+
+class Question<T>(
+    val questionText: String,
+    val answer: T,
+    val difficulty: String
+)
+  
+    fun main() {
+    val question1 = Question<String>("Quoth the raven ___", "nevermore", "medium")
+    val question2 = Question<Boolean>("The sky is green. True or false", false, "easy")
+    val question3 = Question<Int>("How many days are there between full moons?", 28, "hard")
+    
+       println(question1.answer)
+       println(question2.answer)
+       println(question3.answer)
+    
+    
+}
