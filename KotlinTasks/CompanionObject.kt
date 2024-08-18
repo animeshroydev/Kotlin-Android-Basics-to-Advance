53d8a04beed2908eb33a48839a2754cd454f1892
animesh <royanimesh2211@gmail.com>
2024-08-18 23:39:16 +0530

    companion object usecase

---
+++ KotlinTasks/CompanionObject.kt
@@ -0,0 +1,25 @@
+enum class Difficulty {
+    EASY, MEDIUM, HARD
+}
+
+class Question<T>(
+    val questionText: String,
+    val answer: T,
+    val difficulty: Difficulty
+)
+  
+
+class Quiz {
+    val question1 = Question<String>("Quoth the raven ___", "nevermore", Difficulty.MEDIUM)
+    val question2 = Question<Boolean>("The sky is green. True or false", false, Difficulty.EASY)
+    val question3 = Question<Int>("How many days are there between full moons?", 28, Difficulty.HARD)
+
+    companion object StudentProgress {
+        var total: Int = 10
+        var answered: Int = 3
+    }
+}
+
+fun main() {
+    println(Quiz.total)
+}
